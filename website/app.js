91af2fe4437eb93cbea0680d5069a36960096d8a
MaryamMohamed <mariammosa1999@gmail.com>
2021-11-11 07:14:33 +0200

    finish Project Environment Setup

--- website/app.js
+++ website/app.js
@@ -1,4 +1,6 @@
 /* Global Variables */
+const baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '1fa9860c71c3c66313269250521a9a41';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
